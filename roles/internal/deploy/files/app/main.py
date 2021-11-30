ca6ff8412fa0c0a0688578b2fd2a3ded946cf89e
Engin Yöyen <engin.yoyen@gmail.com>
2021-11-30 22:03:26 +0100

    Remove conditonal sig-term and clean up on exit

--- roles/internal/deploy/files/app/main.py
+++ roles/internal/deploy/files/app/main.py
@@ -2,34 +2,35 @@
 import sys
 import signal
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
 from soco import SoCo
 import configparser
+from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 config = configparser.ConfigParser()
+config.read('/home/pi/app/props.ini')
 
-config.read('props.ini')
 speakerIP = config['speaker']['ip']
 look_up_table = config['look-up-table']
-
 speaker = SoCo(speakerIP)
+continue_reading = True
 
 def sigterm_handler(_signo, _stack_frame):
     # Raises SystemExit(0) so it could exit gracefully 
     # This makes sure that GPIO will be cleaned 
+    continue_reading = False
+    GPIO.cleanup()
     sys.exit(0)
 
-if sys.argv[1] == "handle_signal":
-    signal.signal(signal.SIGTERM, sigterm_handler)
-
-while True:
+signal.signal(signal.SIGTERM, sigterm_handler)
+    
+ 
+while continue_reading:
     try:
         id, text = reader.read()
         uri = look_up_table[str(id)]
         speaker.clear_queue()
         speaker.add_uri_to_queue(uri)
         speaker.play_from_queue(0)
-    finally:
-        GPIO.cleanup()
-    
+    except Exception:
+        pass
